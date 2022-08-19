888ec482ff7742881144af502f9875fae5846765
Allen Nelson <ithinkican@gmail.com>
2022-08-19 00:01:21 -0500

    add draftbit/ui and dependencies

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Picker } from "@draftbit/core";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Picker onValueChange={v => console.log(v)}/>
     </View>
   );
 }
